eb07cd24922a103dbca1170cd56271c449156a20
xertifiedloaded <certifiedloaded@yahoo.com>
2024-09-02 20:35:38 +0100

    my changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,8 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>abg</title>
-        <meta name="description" content="The Future Workforce" />
+        <title>Animal Breeding And Genetics</title>
+        <meta name="description" content="Department Of Animal Breeding And Genetics" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -19,7 +19,6 @@ export default function Home() {
         <footer className="text-center mt-4">
           <small className="block text-[12px] mb-1"> Made with <span className="animate-pulse">❤️</span> by Makinde Olaitan (Xertified)</small>
           <small className="block"> Department of Animal Breeding And Genetics {new Date().getFullYear()}</small>
-
         </footer>
       </main>
     </>
