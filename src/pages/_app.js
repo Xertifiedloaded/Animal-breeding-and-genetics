357f49955872199d8e74169fc5350d4cf16019c0
xertifiedloaded <certifiedloaded@yahoo.com>
2024-09-02 20:44:00 +0100

    insight and Analytics inplementned to get number of visitor that visit the the site and condition of the site

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -15,7 +15,6 @@ export default function App({ Component, pageProps }) {
       <div className={`${poppin.className}`}>
         <AuthProvider>
           <ApiProvider>
-      
             <Component {...pageProps} />
           </ApiProvider>
         </AuthProvider>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,7 +1,7 @@
 
 import { Head, Html, NextScript, Main } from "next/document";
 import { Analytics } from "@vercel/analytics/react"
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function RootLayout() {
   return (
@@ -10,6 +10,7 @@ export default function RootLayout() {
       <body>
         <Main />
         <Analytics/>
+        <SpeedInsights/>
         <NextScript />
       </body>
     </Html>
