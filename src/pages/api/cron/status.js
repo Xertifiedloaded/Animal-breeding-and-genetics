d1df694a6363c69aa99a042c29ceecdeb168b0be
xertifiedloaded <certifiedloaded@yahoo.com>
2024-10-10 12:38:12 +0100

    cron job

--- src/pages/api/cron/status.js
+++ src/pages/api/cron/status.js
@@ -1,15 +1,10 @@
 
-export default function handler(request, response) {
-    if (request.method !== 'GET') {
-        return response.status(405).json({ success: false, message: 'Method Not Allowed' });
-    }
-    const authHeader = request.headers['authorization'];
-    if (
-        !process.env.CRON_SECRET ||
-        authHeader !== `Bearer ${process.env.CRON_SECRET}`
-    ) {
-        return response.status(401).json({ success: false, message: 'Unauthorized' });
+export default async function handler(req, res) {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+        return res.status(401).send('Unauthorized');
     }
 
-    response.status(200).json({ message: 'code ran' + new Date() },);
+    return res.status(200).json({ success: true });
 }
